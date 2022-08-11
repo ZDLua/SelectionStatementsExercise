2e0aff22859243d00ce8427c0fc40a0e29a7a9eb
ZDLua <hj_jr@icloud.com>
2022-08-11 10:02:47 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            // create a function that asks the user to guess
+            static void AskUserToGuess()
+             
+            {
+            var r = new Random();
+            var favNumber = r.Next(1,1000);
+
+            Console.WriteLine("Guess my favorite number between 1 and 1000");
+            var guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess == favNumber)
+            {
+                Console.WriteLine("You guessed my favorite number!");
+            }
+            else if (guess > favNumber)
+            {
+                Console.WriteLine("Your guess is too high");
+                AskUserToGuess();
+                
+            }
+            else
+            {
+                Console.WriteLine("Your guess is too low");
+                AskUserToGuess();
+            }
+            
+           
+        }
+        AskUserToGuess();
+        }
+    }
+}
+
